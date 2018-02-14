5725bde665ce8a58ab587fa0cb92bf49e753feac
Corentin Turcaud <corentin.turcaud@gmail.com>
2018-02-14 12:11:53 +0100

    Add style, "replay" button and "verify" function

--- ShowLyrics.js
+++ ShowLyrics.js
@@ -1,16 +1,17 @@
-var test = "";
+var response = "";
 var toShow = "";
-getLyrics("test","test2");
 var t;
+var autor = "autor";
+var song = "song";
 
 function Show(){
-    var n = test.indexOf('\n');
+    var n = response.indexOf('\n');
     if (n == -1){
-        toShow += test;
+        toShow += response;
     	stopShow();
     }
-    toShow += test.substring(0,n+1)+ "<br>";
-    test = test.substring(n+1,test.length);
+    toShow += response.substring(0,n+1)+ "<br>";
+    response = response.substring(n+1,response.length);
     document.getElementById('lyrics').innerHTML = toShow;
 }
 
@@ -31,13 +32,27 @@ function getLyrics(autor, song){
 	req.onreadystatechange = function() { 
         if (req.readyState == 4 && req.status == 200){
             rep=req.responseText;
-            test = JSON.parse(req.response).lyrics;
-            console.log(req)
-                startShow();
+            response = JSON.parse(req.response).lyrics;
+            startShow();
         }
     }
 
 	req.open('GET', address, true); 
 	req.send(null);
+}
 
+function replay(){
+    stopShow();
+    toShow = "";
+    getLyrics();
+}
+
+function verify(){
+    var answer = document.getElementById('reponse').value.toLowerCase();
+    if(answer.indexOf(autor) != -1){
+        console.log("autor bon!")
+    }
+    if(answer.indexOf(song) != -1){
+        console.log("song bon!")
+    }
 }
